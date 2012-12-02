a39f08e966d7d48b55e646142078227bdcec3c70
MunkiPhD <munkiphd@gmail.com>
2012-12-02 16:02:23 -0500

    Initial commit

---
+++ lib/flash-dance.rb
@@ -0,0 +1,7 @@
+require "flash-dance/version"
+
+module Flash
+  module Dance
+    # Your code goes here...
+  end
+end
---
+++ lib/flash-dance/version.rb
@@ -0,0 +1,5 @@
+module Flash
+  module Dance
+    VERSION = "0.0.1"
+  end
+end
